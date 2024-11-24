ce3cc19bbae6ca7b3a2947a01cf5525483da6c98
skvadda1 <156102921+skvadda1@users.noreply.github.com>
2024-11-24 09:49:43 -0500

    Add files via upload

---
+++ first-activity.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    literation = 0
+    print("The total number entered by the user is", n)
+    literation += 1
+    print("Total literation done by computer is",literation,"\n")
+printnumber(10)
+printnumber(200)
+print("\n with any 'n' the time taken by the computer will not change")
